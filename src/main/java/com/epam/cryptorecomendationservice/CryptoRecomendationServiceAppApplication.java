db49f10d39c5384c794ce24d8c0d329d177ac24f
Olha_Sliusarchuk <cherniienko.olya@gmail.com>
2023-04-11 11:12:01 +0200

    Create Spring Boot Archetype

---
+++ src/main/java/com/epam/cryptorecomendationservice/CryptoRecomendationServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.epam.cryptorecomendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRecomendationServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRecomendationServiceAppApplication.class, args);
+	}
+
+}
